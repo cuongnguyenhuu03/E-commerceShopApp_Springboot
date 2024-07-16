3c650089e7a81c16b77ea0f145b5c089298dc756
CuongDepTrai <nguyenhuucuong111103@gmail.com>
2024-07-16 07:25:09 +0700

    add JwtToken, update logic,regist controller

---
+++ src/main/java/com/project/ShopApp/components/JwtTokenUtil.java
@@ -0,0 +1,72 @@
+package com.project.ShopApp.components;
+
+import com.project.ShopApp.models.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenUtil {
+
+    @Value("${jwt.expiration}")
+    private int expiration; // save to an enviroment variable
+
+    @Value("${jwt.secretKey}")
+    private String secretKey;
+
+
+    public String generateToken(User user){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("phoneNumber", user.getPhoneNumber());
+        try {
+            String token  = Jwts.builder()
+                    .setClaims(claims)
+                    .setSubject(user.getPhoneNumber())
+                    .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000L))
+                    .signWith(getSignInkey(), SignatureAlgorithm.HS256)
+                    .compact();
+
+            return token;
+        } catch (Exception e){
+            System.err.println("Can not create jwt token, error: "+e.getMessage());
+            return null;
+        }
+    }
+
+    // convert key from String to Key Object
+    private Key getSignInkey(){
+        byte[] bytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(bytes);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInkey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = this.extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    //check expiration
+    private boolean isTokenExpired(String token){
+        Date expirationDate = this.extractClaim(token, Claims ::getExpiration);
+        return expirationDate.before(new Date());
+    }
+}
--- src/main/java/com/project/ShopApp/services/impl/UserService.java
+++ src/main/java/com/project/ShopApp/services/impl/UserService.java
@@ -1,5 +1,6 @@
 package com.project.ShopApp.services.impl;
 
+import com.project.ShopApp.components.JwtTokenUtil;
 import com.project.ShopApp.dtos.UserDTO;
 import com.project.ShopApp.exceptions.DataNotFoundException;
 import com.project.ShopApp.models.Role;
@@ -9,18 +10,27 @@ import com.project.ShopApp.repositories.UserRepository;
 import com.project.ShopApp.services.IUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService implements IUserService {
 
-    private UserRepository userRepository;
-    private RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenUtil jwtTokenUtil;
+    private final AuthenticationManager authenticationManager;
 
     @Override
     public User createUser(UserDTO userDTO) throws DataNotFoundException {
-
+        // register user
         String phoneNumber = userDTO.getPhoneNumber();
 
         if(userRepository.existsByPhoneNumber(phoneNumber)){
@@ -38,18 +48,37 @@ public class UserService implements IUserService {
                 .build();
         Role role = roleRepository.findById(userDTO.getRoleId())
                 .orElseThrow(() -> new DataNotFoundException("Role Not Found"));
-        newUser.setRoleId(role);
+        newUser.setRole(role);
 
         if(userDTO.getFacebookAccountId() == 0 && userDTO.getGoogleAccountId() == 0){
             String password = userDTO.getPassword();
-            //String encodePassword = passwordEncoder.encode(password);
-            //newUser.setPassword(encodePassword);
+            String encodedPassword = passwordEncoder.encode(password);
+            newUser.setPassword(encodedPassword);
         }
         return userRepository.save(newUser);
     }
 
     @Override
-    public String login(String phoneNumber, String password) {
-        return null;
+    public String login(String phoneNumber, String password) throws DataNotFoundException {
+        Optional<User> optionalUser =  userRepository.findByPhoneNumber(phoneNumber);
+        if(optionalUser.isEmpty()){
+            throw new DataNotFoundException("Invalid phone number / password");
+        }
+        User existingUser = optionalUser.get();
+
+        // check password
+        if(existingUser.getFacebookAccountId() == 0
+                && existingUser.getGoogleAccountId() == 0){
+            if(!passwordEncoder.matches(password, existingUser.getPassword())){
+                throw new BadCredentialsException("Wrong phone number or password");
+            }
+        }
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+            phoneNumber, password
+        );
+
+        //authenticate with Java Spring
+        authenticationManager.authenticate(authenticationToken);
+        return jwtTokenUtil.generateToken(existingUser);
     }
 }
